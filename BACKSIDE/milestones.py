562fd62908f106f9b82cc1689229a4628730be61
Tyler Chen <tyche4490@gmail.com>
2025-09-14 07:23:53 -0400

    pruned diff context

--- BACKSIDE/milestones.py
+++ BACKSIDE/milestones.py
@@ -38,6 +38,7 @@ class RawMilestone:
             "git",
             "--no-pager",
             "diff",
+            "-U0",
             f"{self.start_commit_hash}..{self.end_commit_hash}",
             "--",
             filechange,
